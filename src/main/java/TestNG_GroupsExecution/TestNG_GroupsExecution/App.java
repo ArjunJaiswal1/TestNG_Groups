0fc4bd782e5051686e65a8ece72ae1a44961d8b1
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-11-22 10:29:14 +0545

    GroupsExecution

---
+++ src/main/java/TestNG_GroupsExecution/TestNG_GroupsExecution/App.java
@@ -0,0 +1,13 @@
+package TestNG_GroupsExecution.TestNG_GroupsExecution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
